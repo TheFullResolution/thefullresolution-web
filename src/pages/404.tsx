0ef19112dd8cc626788e5848141d59a77e374af5
Jedrzej <progreacja@gmail.com>
2019-04-25 17:29:40 +0200

    Add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { GoIssueOpened } from 'react-icons/go'
+import { Page } from '../components/Page/Page'
+
+const Page404: React.FC = () => (
+  <Page>
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+      <GoIssueOpened /> 404 PAGE <GoIssueOpened />
+      </h1>
+      <p>{`I am sorry, the page you are looking for, doesn't exists.`}</p>
+    </div>
+  </Page>
+)
+
+export default Page404
