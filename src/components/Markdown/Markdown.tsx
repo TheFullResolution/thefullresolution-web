8cc0fc90a3cde911566279a6aa836378f9364982
Jedrzej <progreacja@gmail.com>
2020-11-30 07:11:31 +0100

    Updates

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import MarkdownToJsx from 'markdown-to-jsx'
-import * as FaIcons from 'react-icons/fa'
-import * as GoIcons from 'react-icons/go'
-import { IconType } from 'react-icons'
-import { ResponsiveImg } from '../ResponsiveImg/ResponsiveImg'
+import React from 'react';
+import MarkdownToJsx from 'markdown-to-jsx';
+import * as FaIcons from 'react-icons/fa';
+import * as GoIcons from 'react-icons/go';
+import { IconType } from 'react-icons';
+import { ResponsiveImg } from '../ResponsiveImg/ResponsiveImg';
 
-const TypedFaIcons = FaIcons as Record<string, IconType>
-const TypedGoIcons = GoIcons as Record<string, IconType>
+const TypedFaIcons = FaIcons as Record<string, IconType>;
+const TypedGoIcons = GoIcons as Record<string, IconType>;
 
 interface Props {
-  className?: string
-  excerpt?: boolean
+  className?: string;
+  excerpt?: boolean;
 }
 
 const ImageWrapper: React.FC<{ alt: string; title: string; src: string }> = ({
@@ -25,12 +25,12 @@ const ImageWrapper: React.FC<{ alt: string; title: string; src: string }> = ({
       alt={alt}
       title={title}
     />
-  )
-}
+  );
+};
 
 const PWrapper: React.FC = ({ children }) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const assertedChildren = children as any
+  const assertedChildren = children as any;
 
   if (
     assertedChildren &&
@@ -40,46 +40,44 @@ const PWrapper: React.FC = ({ children }) => {
   ) {
     // rendering media without p wrapper
 
-    return <>{children}</>
-  } else return <p>{children}</p>
-}
+    return <>{children}</>;
+  } else return <p>{children}</p>;
+};
 
 const ItagWrapper: React.FC<{ children: string }> = ({ children }) => {
-  const Icon = TypedFaIcons[children] ?? TypedGoIcons[children]
-  if (!Icon) return null
+  const Icon = TypedFaIcons[children] ?? TypedGoIcons[children];
+  if (!Icon) return null;
   // console.log(JSON.stringify(ReactIcons, null, 2))
-  return <Icon />
-}
+  return <Icon />;
+};
 
-const SkipTag: React.FC = () => null
+const SkipTag: React.FC = () => null;
 
 export const Markdown: React.FC<Props> = ({
   className,
   children,
   excerpt = false,
 }) => {
-  const overrides = excerpt
-    ? {
-        img: SkipTag,
-        i: ItagWrapper,
-        h1: SkipTag,
-        h2: SkipTag,
-      }
-    : {
-        img: ImageWrapper,
-        p: PWrapper,
-        i: ItagWrapper,
-      }
-
   return (
     <div className={className}>
       <MarkdownToJsx
         options={{
-          overrides,
+          overrides: excerpt
+            ? {
+                img: SkipTag,
+                i: ItagWrapper,
+                h1: SkipTag,
+                h2: SkipTag,
+              }
+            : {
+                img: ImageWrapper,
+                p: PWrapper,
+                i: ItagWrapper,
+              },
         }}
       >
         {children}
       </MarkdownToJsx>
     </div>
-  )
-}
+  );
+};
